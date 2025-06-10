aa2588f479914e41f3c8994be994c0f40ad52249
ErikH <510637@student.fontys.nl>
2025-06-10 15:31:25 +0200

    Fixed linting issues.

--- amazoff-fe/src/middleware.ts
+++ amazoff-fe/src/middleware.ts
@@ -1,8 +1,10 @@
 import { withAuth } from "next-auth/middleware";
 
+
 export default withAuth(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   function middleware(req) {
-    // Add any custom middleware logic here if needed
+
   },
   {
     callbacks: {
